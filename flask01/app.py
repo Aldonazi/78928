59af2e0e3dfb0a4ee0cc702b8d91a498920fd356
Tu Nombre <zs201021683@estudiantes.uv.mx>
2025-03-07 10:41:18 -0600

    rutas de flask

--- flask01/app.py
+++ flask01/app.py
@@ -1,7 +1,26 @@
-from flask import Flask 
-app = Flask (__name__)
+from flask import Flask
+
+app = Flask(_name_)
 @app.route('/')
 def hola_mundo():
-    return 'hola mundo'
-if __name__== '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    return "hola mundo"
+
+@app.route('/adios')
+def adios_mundo():
+    return "adios mundo"
+
+@app.route('/hola')
+def hola_html():
+    return "<h1 style=color:red>hola html</h1>"
+
+@app.route('/json')
+def algo():
+    return '{"nombre":"john"}'
+
+@app.route('/xml')
+def xml():
+    return '<nombre>john</nombre>'
+
+if _name== "main_":
+    app.run(host='0.0.0.0',
+debug=True)
